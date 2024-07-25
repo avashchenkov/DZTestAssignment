1f48b57fb69b99847cfe7f74121b39ff89f6a0ec
Artur Vashchenkov <washenkov@gmail.com>
2024-07-25 17:53:44 +0200

    Setting up the DB stuff. In progress

---
+++ task2/src/main/java/ru/doczilla/task2/controller/StudentController.java
@@ -0,0 +1,40 @@
+package ru.doczilla.task2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+import ru.doczilla.task2.model.Student;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @PostMapping
+    public void addStudent(@RequestBody Student student) {
+        String sql = "INSERT INTO students (first_name, last_name, middle_name, birth_date, group_name) VALUES (?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, student.getFirstName(), student.getLastName(), student.getMiddleName(), student.getBirthDate(), student.getGroupName());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable("id") Long id) {
+        String sql = "DELETE FROM students WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    @GetMapping
+    public List<Student> listStudents() {
+        return jdbcTemplate.query("SELECT * FROM students", (rs, rowNum) ->
+                new Student(
+                        rs.getLong("id"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getString("middle_name"),
+                        rs.getDate("birth_date").toLocalDate(),
+                        rs.getString("group_name")
+                ));
+    }
+}
---
+++ task2/src/main/java/ru/doczilla/task2/model/Student.java
@@ -0,0 +1,60 @@
+package ru.doczilla.task2.model;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private LocalDate birthDate;
+    private String groupName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+}
